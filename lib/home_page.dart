28db69b118beeace080d4edb233576905e886fd0
AND3FELIPE <afsilva6236@misena.edu.co>
2023-09-10 04:06:18 -0500

    UPDATE : interfaz UI de la aplicacion movil terminada

---
+++ lib/home_page.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+import 'package:voice_assistant_felipe_silva/feature_box.dart';
+import 'package:voice_assistant_felipe_silva/palette.dart';
+
+// stfw --> estate full widget
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Voice Assistant'),
+        leading: const Icon(Icons.menu),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          // virtual assistant picture
+          Stack(
+            children: [
+              Center(
+                child: Container(
+                  height: 120,
+                  width: 120,
+                  margin: const EdgeInsets.only(top: 4),
+                  decoration: const BoxDecoration(
+                    color: Palette.assistantCicleColor,
+                    shape: BoxShape.circle,
+                  ),
+                ),
+              ),
+              Container(
+                height: 123,
+                decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                        image: AssetImage(
+                      "assets/images/virtualAssistant.png",
+                    ))),
+              )
+            ],
+          ),
+          // Chat dobble
+          Container(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 10,
+            ),
+            margin: const EdgeInsets.symmetric(horizontal: 40).copyWith(
+              top: 30,
+            ),
+            decoration: BoxDecoration(
+                border: Border.all(
+                  color: Palette.borderColor,
+                ),
+                borderRadius:
+                    BorderRadius.circular(20).copyWith(topLeft: Radius.zero)),
+            child: const Padding(
+              padding: EdgeInsets.symmetric(vertical: 10.0),
+              child: Text(
+                'Goog morning what task con I do for you?',
+                style: TextStyle(
+                    color: Palette.mainFontColor,
+                    fontSize: 25,
+                    fontFamily: 'Cera Pro'),
+              ),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(10),
+            margin: const EdgeInsets.only(top: 10, left: 30),
+            alignment: Alignment.centerLeft,
+            child: const Text(
+              'Here are a few features',
+              style: TextStyle(
+                  color: Palette.mainFontColor,
+                  fontFamily: 'Cera Pro',
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+          // features list
+          const Column(
+            children: [
+              FeatureBox(
+                  color: Palette.firstSuggestionBoxColor,
+                  headerText: 'ChatGPT',
+                  descriptionText:
+                      'A smarter way to arganized and informed with ChatGPT'),
+              FeatureBox(
+                  color: Palette.secondSuggestionBoxColor,
+                  headerText: 'Dall-E',
+                  descriptionText:
+                      'Get inspired and stay creative with your personal assistant powered by Ball-E'),
+              FeatureBox(
+                  color: Palette.thirdSuggestionBoxColor,
+                  headerText: 'Smart Voice Assistant',
+                  descriptionText:
+                      'Get the best of both worlds with a voice assistant powered by Dall-E and ChatGPT'),
+            ],
+          )
+        ],
+      ),
+      // boton para grabar audio
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Palette.blackColor,
+        onPressed: () {},
+        child: const Icon(
+          Icons.mic,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:voice_assistant_felipe_silva/home_page.dart';
+import 'package:voice_assistant_felipe_silva/palette.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,59 +15,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Hola xd hace'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      theme: ThemeData.light(useMaterial3: true).copyWith(
+          scaffoldBackgroundColor: Palette.whiteColor,
+          appBarTheme: const AppBarTheme(backgroundColor: Palette.whiteColor)),
+      home: const HomePage(),
     );
   }
 }
